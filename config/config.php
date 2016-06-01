4416e10fd47e0ca96df40d2905dc9780a52922dc
p4ram0rph <izicthegreat@gmail.com>
2016-06-01 05:58:35 +0100

    fixed spelling errors

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ class config{
 
          * Should probably separate the debug stuff into a different class 
          * so we can do something like if( System::DEBUG ) { config::Set('username', 'root')}
-         * and etc so only root account if used when debugging is one         
+         * and etc so only root account i used when DEBUG is true     
          */
         
 }
